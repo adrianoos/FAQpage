fed8ee10943f95bb137783879b6d818d5efa99a4
Adrian <adriann.kowalski@gmail.com>
2021-08-08 16:05:40 +0200

    added data to external file in components, created main view mockup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import questions from './components/data'
+import './App.css'
 
 function App() {
+
   return (
     <div className="App">
-Hello
+      <h1>FAQ Page</h1>
+      <form>
+        <input className='MainInput'></input>
+        <button>search</button>
+      </form>
+     <div className='QuestionsDisplay'></div>
+     <p>Place for data display</p>
     </div>
   );
 }
