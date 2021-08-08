b2af0c6d78612788369cad2e16d6dec230118b68
Adrian <adriann.kowalski@gmail.com>
2021-08-08 16:41:02 +0200

    added state and function to store value of input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import data from './components/data'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+
+  const [ query, setQuery ] = useState('')
+  const [ inpValue, setInpValue ] = useState('')
+
+  console.log(inpValue)
+
+  const updateInputValue = (e) => {
+    setInpValue(e.target.value)
+  }
+
   return (
     <div className="App">
       <h1>FAQ Page</h1>
       <form>
-        <input className='MainInput'></input>
+        <input className='MainInput' onChange={updateInputValue}></input>
         <button>search</button>
       </form>
      <div className='QuestionsDisplay'></div>
