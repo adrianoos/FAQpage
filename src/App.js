482e9a275cfdeb881b51b94ba9a3e7269a0c9ec9
Adrian <adriann.kowalski@gmail.com>
2021-08-09 19:20:26 +0200

    added buttons for filtering questions by groups

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
 
   const [ inpValue, setInpValue ] = useState(' ')
   const [ filtered , setFiltered ] = useState(data.questions)
-  const [ showAnswer , setShowAnswer ] = useState('')
+  const [ showAnswer, setShowAnswer ] = useState('')
+  const [ groupDisplay, setGroupDisplay ] = useState('All')
 
   const updateInputValue = (e) => {
     e.preventDefault()
@@ -28,6 +29,17 @@ function App() {
   } else { setShowAnswer('') }
   };
 
+  const switchGroupDisplay = (id) => {
+    if (id == 'All') {
+      setFiltered(data.questions)
+      setGroupDisplay('All')
+    }
+    else { setFiltered(data.questions.filter(item => item.groupId == id))
+           setGroupDisplay(id)
+    }
+
+  };
+
   return (
     <div className="App">
       <h1>FAQ Page</h1>
@@ -35,11 +47,19 @@ function App() {
         <input className='MainInput' onChange={(e) => updateInputValue(e)}></input>
         <button>search</button>
       </form>
+         <div id='questionGroups'>
+           <button className={groupDisplay == 'All' ? 'groupsButtonFilled' : 'groupsButton'} onClick={() => switchGroupDisplay('All')}>All</button>
+           <button className={groupDisplay == 2 ? 'groupsButtonFilled' : 'groupsButton'} onClick={() => switchGroupDisplay(data.groups.left[0].id)}>{data.groups.left[0].name}</button>
+           <button className={groupDisplay == 5 ? 'groupsButtonFilled' : 'groupsButton'} onClick={() => switchGroupDisplay(data.groups.left[1].id)}>{data.groups.left[1].name}</button>
+           <button className={groupDisplay == 1 ? 'groupsButtonFilled' : 'groupsButton'} onClick={() => switchGroupDisplay(data.groups.right[0].id)}>{data.groups.right[0].name}</button>
+           <button className={groupDisplay == 4 ? 'groupsButtonFilled' : 'groupsButton'} onClick={() => switchGroupDisplay(data.groups.right[1].id)} >{data.groups.right[1].name}</button>
+           <button className={groupDisplay == 3 ? 'groupsButtonFilled' : 'groupsButton'} onClick={() => switchGroupDisplay(data.groups.right[2].id)}>{data.groups.right[2].name}</button>
+         </div>
      <div className='QuestionsDisplay'>
      {filtered.map( item =>
-       <div>
+       <div key={item.id}>
             <h1 className='questionParagraph' onClick={() => changeDisplay(item.id)} id={item.id} key={item.title}>{item.title}</h1>
-            {showAnswer == item.id ? <div dangerouslySetInnerHTML={{__html: item.content}}></div> : ''}
+            {showAnswer == item.id ? <div key={item.id} dangerouslySetInnerHTML={{__html: item.content}}></div> : ''}
       </div>)}
      </div>
     </div>
